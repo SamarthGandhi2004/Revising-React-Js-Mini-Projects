7f565f0641894b1f501fe5848a575de10c506065
SamarthGandhi2004 <samarthgandhi30@gmail.com>
2025-07-24 06:42:00 +0530

    .env updated

--- FireBaseContactBook/src/DataBase/DataBase.jsx
+++ FireBaseContactBook/src/DataBase/DataBase.jsx
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDSG6ZCbkJj3TGM0JNM81cIEFOhkZmk-fw",
-  authDomain: "fir-contactbook-5b6fb.firebaseapp.com",
-  databaseURL: "https://fir-contactbook-5b6fb-default-rtdb.firebaseio.com",
-  projectId: "fir-contactbook-5b6fb",
-  storageBucket: "fir-contactbook-5b6fb.firebasestorage.app",
-  messagingSenderId: "204382492108",
-  appId: "1:204382492108:web:5c70a7889d0fbc4cdb7969",
-  measurementId: "G-MW4TNW2MRF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const  db=getFirestore(app);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
